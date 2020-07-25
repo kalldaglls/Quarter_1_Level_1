71512de46cb10a23839d56503983d18c35b4f6db
kalldaglls <kalldaglls@mail.ru>
2020-07-25 18:15:39 +0300

    25/07/20 18:15 Созадил поле field.

---
+++ DZ/src/TicTac.java
@@ -0,0 +1,9 @@
+public class TicTac {
+    public static void main(String[] args) {
+        String [][] field = new String[][] {
+                {"-", "-", "-" },
+                {"-", "-", "-" },
+                {"-", "-", "-" }
+        };
+    }
+}
