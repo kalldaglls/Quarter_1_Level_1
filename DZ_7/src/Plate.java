becb6480479a5182be71b605c9f96c048aa6e10a
kalldaglls <kalldaglls@mail.ru>
2020-08-06 17:34:36 +0300

    Lesson 7: 1) Сделать нормальное форматирование; 2) Сделать метод добавления еды. Выполнено частично: метод сделан, но как после наполнения тарелки опять кормить кота, которому не хватило еды?solved++ 3) Эффективно ли все написано?

--- DZ_7/src/Plate.java
+++ DZ_7/src/Plate.java
@@ -15,14 +15,24 @@ public class Plate {
             System.out.println(name + " " + "is full" + " " + m);
         }
         else {
-            m = false;
-            System.out.println(name + " " + "can't eat:too little food!" + " " + m);//1. Как вставить getAppetite?2. Как заставить plate после
-            //eat в этом случае не показываться?
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Want you fill the plate for" + " " + name + "?");
-            if (scanner.nextLine().equals("yes")) {
-                fillPlate();
+            while (food < n) {
+                m = false;
+                System.out.println(name + " " + "can't eat:too little food!" + " " + m);//1. Как вставить getAppetite?2. Как заставить plate после
+                //eat в этом случае не показываться?
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Want you fill the plate for" + " " + name + "?");
+                if (scanner.nextLine().equals("yes")) {
+                    fillPlate();//Как можно после fillPlate опять по
+                    info();
+                } else break;
+                if (food >= n) {
+                    food -= n;
+                    m = true;
+                    System.out.println(name + " " + "is full" + " " + m);
+                    break;
+                }
             }
+
         }
     }
     public void info() {
